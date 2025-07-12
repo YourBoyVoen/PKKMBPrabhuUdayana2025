490a8b6030e55904d6d17e2309a3d96447542565
YourBoyVoen <zaenkiva08@gmail.com>
2025-07-13 03:11:41 +0800

    set up awal

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-200">
+      <h1 className="text-4xl font-bold text-black">Tailwind aktif! 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
