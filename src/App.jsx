c2d37bbf71dca1f97565e78a07150b3e8f0d0f06
YourBoyVoen <zaenkiva08@gmail.com>
2025-07-16 22:54:46 +0800

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Pkkmb from './pages/pkkmb';
+import Fakultas from './pages/fakultas';
 
 function App() {
   return (
-    <>
-    <Home />
-    <Pkkmb />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pkkmb" element={<Pkkmb />} />
+        <Route path="/fakultas" element={<Fakultas />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,24 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
 export default function Navbar() {
   return (
     <nav className="w-full fixed top-0 z-50 flex justify-between items-center px-8 py-4 bg-white shadow-md">
       <div className="flex items-center gap-2">
-       <div className="flex items-center gap-2">
         <img src="/assets/images/logopkkmb.png" alt="Logo PKKMB" className="w-8 h-8 object-contain" />
-        <span className="font-bold font-poppins text-lg text-gray-700">PKKMB Prabhu Udayana 2025</span>
+        <Link to= "/" className="font-bold font-poppins text-lg text-gray-700">
+          PKKMB Prabhu Udayana 2025
+        </Link>
       </div>
-      </div>
+
       <ul className="flex gap-8 text-gray-700 font-medium font-poppins">
-        <li><a href="#" className="hover:text-blue-500 transition">Warta Ksatria</a></li>
-        <li><a href="#" className="hover:text-blue-500 transition">Jelajah Udayana</a></li>
-        <li><a href="#" className="hover:text-blue-500 transition">Ormawa</a></li>
+        <li><Link to="/" className="hover:text-blue-500 transition">Warta Ksatria</Link></li>
+        <li><Link to="/pkkmb" className="hover:text-blue-500 transition">Jelajah Udayana</Link></li>
+        <li><Link to="/informasi" className="hover:text-blue-500 transition">Informasi</Link></li>
+        <li><Link to="/fakultas" className="hover:text-blue-500 transition">Fakultas</Link></li>
+        <li><Link to="/ormawa" className="hover:text-blue-500 transition">Ormawa</Link></li>
       </ul>
+      
     </nav>
   );
 }
